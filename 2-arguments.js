698d59c41bb4a669b4b1923e05f57c85c833f446
BRIAN-cloud-cyber <omondiomimo@gmail.com>
2025-11-09 23:34:45 +0300

    js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,2 +1,11 @@
-let arg1 = " ";
- if arg1 ==" ";
+ 
+const args = process.argv.length - 2;
+
+if (args === 0) {
+  console.log('No argument');
+} else if (args === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
+
